583c8118aab2fa09469ff6b229b655892229a4a2
lukalazovic99 <luka.lazovic7@gmail.com>
2024-03-03 02:07:11 +0100

    assistant function2

--- pythonBack/assistant.py
+++ pythonBack/assistant.py
@@ -18,18 +18,17 @@ def parse_response(resp):
     return info
 
 
-def grazie_assistant(client_ip, token, task_text, new_content):
+def grazie_assistant(client_ip, token, task_text, new_content, context):
     pre_setting = "You are teaching an aspiring programmer with he is trying to finish this Programming Course Task(Give Answers in bullet points):"
     setting = pre_setting + task_text
 
-    context = "None"
     format = "Format of your answer should be: " \
              "1) section <<HINTs>> with Independent Numbered Bullet points (that should all be independent from one another) " \
              "that should not spoil the answer to the coder " \
              "2) section <<QUESTIONS TO GET THE ANSWER EASIER>>  " \
              "3) section <<TIPS>> with Independent Numbered Bullet points (that should all be independent from one another) " \
-             "4) section <<TIME COMPLEXITY>> Where you should ONLY give tips on how to decrease time complexity if possible" \
-             "5) section <<SPACE COMPLEXITY>> Where you should ONLY give tips on how to decrease space complexity if possible" \
+             "4) section <<TIME COMPLEXITY>> Where you should ONLY(no need to tell me current complexity if its good) give tips on how to decrease time complexity if possible(with Independent Numbered Bullet points (that should all be independent from one another))" \
+             "5) section <<SPACE COMPLEXITY>> Where you should ONLY(no need to tell me current complexity if its good) give tips on how to decrease space complexity if possible(with Independent Numbered Bullet points (that should all be independent from one another))" \
              "6) section <<CORRECT ANSWER:>> with corrected code. "
 
     whole_prompt = "Your setting: " + setting \
@@ -98,6 +97,87 @@ public: \
     }\
 };"
 
-grazie_assistant(cl_ip, tokn, tekst_zadatka, pokusaj_resenja)
+grazie_assistant(cl_ip, tokn, tekst_zadatka, pokusaj_resenja, "None")
 
 
+tekst_zadatka="Write a function to find the longest common prefix string amongst an array of strings.\
+If there is no common prefix, return an empty string ""."
+
+pokusaj_resenja='''#include <bits/stdc++.h>
+using namespace std;
+
+string findPrefix(string x, string y)
+{
+    //base case checking
+    if (x == "" || y == "")
+        return "";
+
+    string result = "";
+
+    //if string x is smaller than y
+    if (x.length() < y.length()) {
+        //check up for common prefix part
+        for (int i = 0; i < x.length(); i++) {
+            if (x[i] == y[i])
+                result += x[i];
+        }
+    }
+    else {
+        //if string y is smaller than x
+        for (int i = 0; i < y.length(); i++) {
+            //check up for common prefix part
+            if (y[i] == x[i])
+                result += y[i];
+            else
+                return result;
+        }
+    }
+    return result;
+}
+
+string longestCommonPrefix(vector<string>& strs)
+{
+    //base cases
+    if (strs.size() == 0)
+        return "";
+    //if only one string exists
+    if (strs.size() == 1)
+        return strs[0];
+    string prefix = strs[0];
+    //follow the associative property for checking
+    //take two strings each time & pass output prefix
+    //as one string for further processings
+    for (int i = 1; i < strs.size(); i++) {
+        prefix = findPrefix(prefix, strs[i]);
+        if (prefix == "")
+            return prefix;
+    }
+    return prefix;
+}
+
+int main()
+{
+    int n;
+    cout << "enter no of strings you want to add\n";
+    cin >> n;
+
+    string s;
+    vector<string> v;
+    cout << "enter " << n << " strings\n";
+
+    //collect input
+    while (n--) {
+        cin >> s;
+        v.push_back(s);
+    }
+    //print longest common prefix
+    if (longestCommonPrefix(v) == "")
+        cout << "no common prefix between the strings\n";
+    else
+        cout << "longest common prefix: " << longestCommonPrefix(v) << endl;
+
+    return 0;
+}'''
+
+
+grazie_assistant(cl_ip, tokn, tekst_zadatka, pokusaj_resenja, "None")
